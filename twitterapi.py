3568a9a0e6baa9698f924ae85eecde49154e1c45
kumaryas <yashk8419@gmail.com>
2024-12-13 09:20:54 +0530

    fixed this 89 and 401 issue

--- twitterapi.py
+++ twitterapi.py
@@ -6,7 +6,6 @@ consumer_key = "PLh4yR6IRL06ey0t5eDuCisNy"
 consumer_secret = "D0dyjpV5nySsfUgApr0sOey285yVj3gMWbPfCM6Uf1OBcQvQOF"
 access_token ="1411971933025959939-7wJ4EY1eNWY0N5Dfc0uliEHSYBjcwV"
 access_token_secret = "mODXFPBldy8As7pPDkrbpQxJlkqPCbvkuy37Fxe6Xn3hE"
-
 auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
 url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 params = {"screen_name": "yash19014055", "count": 5}
